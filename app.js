c2b40ab2987859140b6625a2c4610f84cb7a73a7
saman2620 <102049962+saman2620@users.noreply.github.com>
2022-03-25 20:24:06 +0500

    Add files via upload
    1st Project Completed

---
+++ app.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('.form');
+const radioInputs = document.querySelectorAll('.form__radio-input');
+const ratingCard = document.querySelector('.rating-card');
+
+let ratingValue;
+
+radioInputs.forEach(input => {
+    input.addEventListener("change", (e) =>{
+    ratingValue = e.target.value;
+    });
+});
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    if(!ratingValue) return;
+renderThankyouContent(ratingCard, ratingValue);
+});
+ 
+
+function renderThankyouContent(container, rateValue) {
+    container.innerHTML =`
+    <div class="thank-you">
+    <img
+     src="./assesst/images/illustration-thank-you.svg" alt="thank you" class="thank-you-img"/>
+ 
+<p class="result"> You selected ${ratingValue} out of 5</p>
+<h1 class="thank-you__title">Thank You!</h1>
+<p class="thank-you__description">We appreciate you taking
+    the time to give a rating. If you ever need more support,
+    don't hesitate to get in touch!
+</p>
+</div>
+`;
+}
